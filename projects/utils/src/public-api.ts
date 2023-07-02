41de0472d161ea44b8f11bff7e8d11f4f2652c0c
madamcin <matej.adamcin@infobip.com>
2023-07-02 18:56:08 +0200

    module federation app

---
+++ projects/utils/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of utils
+ */
+
+export * from './lib/utils.service';
+export * from './lib/header/header.component';
+export * from './lib/utils.module';
+export * from './lib/mfe.utils'
